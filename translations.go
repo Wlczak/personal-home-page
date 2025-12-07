4699301e9d070e45af08dc4c2cef543f308e745b
Adam Vlček <vlcek-adam@seznam.cz>
2025-12-07 10:41:21 +0100

    Added translations

--- translations.go
+++ translations.go
@@ -7,5 +7,20 @@ func getTranslations() map[string]MultiLangString {
 			Cs: "Moje projekty",
 			Jp: "私のプロジェクト",
 		},
+		"Language": {
+			En: "Language",
+			Cs: "Jazyk",
+			Jp: "言語",
+		},
+		"IndexSubHeading": {
+			En: "A collection of my recent work and experiments",
+			Cs: "Sbírka mých nedávných projektů a experimentů",
+			Jp: "僕の最近の仕事と実験のコレクション",
+		},
+		"StatusPage": {
+			En: "Status page",
+			Cs: "Stav stránky",
+			Jp: "ステータスページ",
+		},
 	}
 }

6f10866746677e478c8d1935a1648febad68db0b
Adam Vlček <vlcek-adam@seznam.cz>
2026-01-05 19:01:45 +0100

    added japanese translations

--- translations.go
+++ translations.go
@@ -5,26 +5,31 @@ func getTranslations() map[string]MultiLangString {
 		"AboutMeHeading": {
 			En: "Hi I'm Adam Vlček",
 			Cs: "Ahoj, jsem Adam Vlček",
-			// Ja: "こんにちは、Adam Vlček",
+			Ja: "こんにちは、Adam Vlčekです",
 		},
 		"AboutMeSubheading": {
 			En: "a junior backend software developer",
 			Cs: "junior backend software developer",
+			Ja: "ジュニアバックエンドエンジニア",
 		},
 		"AboutMe1": {
 			En: `I'm a developer based in Pilsen, Czechia. I mostly build backends for web applications, but I also enjoy working 
 			with embedded systems for their straightforward, hands-on nature.`,
 			Cs: `Jsem Plzeňský vývojář. Nejčastěji tvořím backendy pro webovky, ale rád pracuji třeba i s embedded systémy kvůli příme práci s hardwarem bez abstrakcí.`,
+			Ja: `チェコのピルゼン（Plzeň）を拠点にしている開発者です。主にウェブアプリケーションのバックエンドを構築していますが、直接的で実践的な性質が好きなので組み込みシステムの開発も楽しんでいます。`,
 		},
 		"AboutMe2": {
 			En: `I primarily use PHP and Go for my personal projects, though I like trying out other languages from time
                 to time. I'm currently interested in learning Rust and maybe Kotlin for native android development.`,
 			Cs: `Ve svých projektech nejčastěji využívám jazyky PHP a Go, ale rád zkouším a učím se práci s novými jazyky, či technologiemi. Aktuálně mám zájem se naučit Rust a možná Kotlin pro nativní android vývoj.`,
+			Ja: `個人プロジェクトでは主にPHPとGoを使っていますが、時々他の言語を試すのも好きです。現在はRustを学びたいと考えており、ネイティブAndroid開発のためにKotlinも学んでみたいと思っています。`,
 		},
 		"MainUsedLanguages": {
 			En: "My main languages",
 			Cs: "Moje nejpoužívanější jazyky",
+			Ja: "主に使っている言語",
 		},
+
 		"ViewMyGitHub": {
 			En: "View GitHub",
 			Cs: "Zobrazit GitHub",
